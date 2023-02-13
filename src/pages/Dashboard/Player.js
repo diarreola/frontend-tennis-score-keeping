d4adb62c571aa31185fab261696af62b7dfc58fb
diana <arreoladiana11@gmail.com>
2023-02-12 19:48:24 -0800

    Add Player list

--- src/pages/Dashboard/Player.js
+++ src/pages/Dashboard/Player.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Player(props) {
+function Player({id, firstName, lastName}) {
 
   return (
-
+    <div>
+      
+      <li className="board-title">{firstName} {lastName}, id: {id}</li>
+    </div>
   );
 }
+
+Player.propTypes = {
+  id: PropTypes.number.isRequired,
+  firstName: PropTypes.string.isRequired,
+  lastName: PropTypes.string.isRequired,
+};
 
 export default Player;
