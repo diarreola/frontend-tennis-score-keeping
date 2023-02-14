9c113603b9e2905f7f6bb88982a331e979ec4b88
diana <arreoladiana11@gmail.com>
2023-02-14 09:58:14 -0800

    Add sign in form

--- src/pages/Auth/SignIn.js
+++ src/pages/Auth/SignIn.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import SignInForm from './SignInForm';
 
 const SignIn = () => {
   return (
-    <div>
-      Sign In
-    </div>
+    <Container>
+        <Row>
+          <Col xs={6} md={4}>
+          </Col>
+          <Col xs={6} md={4}>
+            <Card>
+              <Card.Header>Sign In</Card.Header>
+              <Card.Body>
+                <SignInForm />
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col xs={6} md={4}>
+          </Col>
+        </Row>
+    </Container>
   )
 }
 
--- src/pages/Auth/SignInForm.js
+++ src/pages/Auth/SignInForm.js
@@ -3,25 +3,14 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import {Link} from 'react-router-dom';
 
-const SignUpForm = () => {
+const SignInForm = () => {
   return (
     <div>
       <Form>
-        <p>Already have an account? <Link to='/' className='underline'>Sign in.</Link></p>
-        <Form.Group className="mb-3" controlId="formFirstName">
-          <Form.Label>First Name</Form.Label>
-          <Form.Control required type="text" placeholder="First Name" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formLastName">
-          <Form.Label>Last Name</Form.Label>
-          <Form.Control required type="text" placeholder="Last Name" />
-        </Form.Group>
+        <p>Don't have an account? <Link to='/signup' className='underline'>Sign up.</Link></p>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control required type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
@@ -35,4 +24,4 @@ const SignUpForm = () => {
   )
 }
 
-export default SignUpForm
+export default SignInForm
