946c2a91abbb0a293b48d6870928ea0c4c2df779
diana <arreoladiana11@gmail.com>
2023-02-16 20:18:58 -0800

    Add current match buttons

--- src/pages/CurrentMatch/CurrentMatch.js
+++ src/pages/CurrentMatch/CurrentMatch.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import UserNavbar from '../../components/UserNavbar';
+import MatchButtons from './MatchButtons';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import MatchControlFooter from './MatchControlFooter';
 
 const CurrentMatch = () => {
   return (
     <div>
       <UserNavbar></UserNavbar>
-
-
+      <Container>
+        <Row>
+          <Col>1 of 1</Col>
+        </Row>
+        <Row>
+          <Col><MatchButtons /></Col>
+          <Col><MatchButtons /></Col>
+        </Row>
+      </Container>
       <MatchControlFooter></MatchControlFooter>
     </div>
   )
