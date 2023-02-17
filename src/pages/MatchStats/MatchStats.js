500ef0d490cbb8813057a07f4b0dc5208708b5a7
diana <arreoladiana11@gmail.com>
2023-02-16 18:24:06 -0800

    MAtch stats table created

--- src/pages/MatchStats/MatchStats.js
+++ src/pages/MatchStats/MatchStats.js
@@ -8,31 +8,51 @@ const MatchStats = ({onHandleLogout}) => {
     <section>
       <UserNavbar></UserNavbar>
       <Table striped bordered hover variant="dark">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Username</th>
-          </tr>
-        </thead>
         <tbody>
           <tr>
-            <td>1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
+            <td>Player1</td>
+            <th></th>
+            <td>Player 2</td>
           </tr>
           <tr>
             <td>2</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
+            <th>Aces</th>
+            <td>3</td>
           </tr>
           <tr>
+            <td>1</td>
+            <th>Double Faults</th>
+            <td>0</td>
+          </tr>
+          <tr>
+            <td>0</td>
+            <th>U.Errors</th>
+            <td>2</td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <th>F.Errors</th>
             <td>3</td>
-            <td colSpan={2}>Larry the Bird</td>
-            <td>@twitter</td>
+          </tr>
+          <tr>
+            <td>5</td>
+            <th>Wins</th>
+            <td>2</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <th>Sets Won</th>
+            <td>1</td>
+          </tr>
+          <tr>
+            <td>41</td>
+            <th>Age</th>
+            <td>40</td>
+          </tr>
+          <tr>
+            <td>15.9</td>
+            <th>UTR</th>
+            <td>14.9</td>
           </tr>
         </tbody>
       </Table>
